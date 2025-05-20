c754295f64627e85c325b062a11ca267d76e03af
matinrad <keyvanmatinrad@outlook.com>
2025-05-20 14:44:49 +0330

    Add react-hook-form for form handling and enhance button loading state
    - Integrated `react-hook-form` for improved form management in blog creation and editing forms, allowing for better validation and state handling.
    - Updated the button component to display a loading spinner when the form is being submitted, enhancing user experience during asynchronous operations.
    - Added a new primary color variable in CSS for consistent styling across the application.

--- app/(root)/admin/blogs/[id]/edit/page.tsx
+++ app/(root)/admin/blogs/[id]/edit/page.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react';
 import { Blog } from '@/lib/api/panel/admin/blogs';
 import { Toaster } from 'react-hot-toast';
 import { use } from 'react';
+import LoadingSpinner from '@/components/ui/loading-spinner';
 
 export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const resolvedParams = use(params);
@@ -36,7 +37,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
   }, [resolvedParams?.id]);
 
   if (loading) {
-    return <div>در حال بارگذاری...</div>;
+    return <LoadingSpinner />;
   }
 
   if (!blog) {
